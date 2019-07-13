502f822530f38f4b5cc4588f967ccc24fe056eb2
David <davidschulte1993@gmx.net>
2019-07-13 13:21:53 +0200

    Bug Fix

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ np.set_printoptions(threshold=sys.maxsize)
 
 args = dotdict({
     'numIters': 1000,
-    'numEps': 1,
+    'numEps': 20,
     'tempThreshold': 15,
     'updateThreshold': 0.6,
     'maxlenOfQueue': 200000,
-    'numMCTSSims': 10,
+    'numMCTSSims': 15,
     'arenaCompare': 6,
     'cpuct': 40,
 
