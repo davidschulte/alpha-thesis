e861833840abb5b14ed82ab687e3810471a852b0
David <davidschulte1993@gmx.net>
2019-07-28 00:03:00 +0200

    Start with greedy examples

--- Coach.py
+++ Coach.py
@@ -46,7 +46,6 @@ class Coach():
 
         self.greedy_actor = VeryGreedyActor(game)
 
-    @profile
     def executeEpisode(self, first):
         """
         This function executes one episode of self-play, starting with player 1.
@@ -156,7 +155,7 @@ class Coach():
                     # bookkeeping + plot progress
                     eps_time.update(time.time() - end)
                     end = time.time()
-                    bar.suffix  = '({eps}/{maxeps}) Eps Time: {et:.3f}s | Total: {total:} | ETA: {eta:}'.format(eps=eps+1, maxeps=self.args.numEps, et=eps_time.avg,
+                    bar.suffix  = '({eps}/{maxeps}) Eps Time: {et:.3f}s | Total: {total:} | ETA: {eta:}'.format(eps=eps+1, maxeps=num_eps, et=eps_time.avg,
                                                                                                                total=bar.elapsed_td, eta=bar.eta_td)
                     bar.next()
                 bar.finish()
