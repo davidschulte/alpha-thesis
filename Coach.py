9b9adc73b1d27d2833f3bd00f83bb5aaf3e1c513
David <davidschulte1993@gmx.net>
2019-07-29 16:55:38 +0200

    Prevent Loops

--- Coach.py
+++ Coach.py
@@ -143,7 +143,7 @@ class Coach():
 
                 num_eps = self.args.numEps
                 if i == 1 and not self.args.load_model:
-                    num_eps = 500
+                    num_eps = 1000
                 eps_time = AverageMeter()
                 bar = Bar('Self Play', max=num_eps)
                 end = time.time()
